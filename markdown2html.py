b451eed5d1f35091c4fe846c8e3ab8fbb8fde657
Jocseh07 <josephmkaranja07@gmail.com>
2024-08-22 09:49:03 +0300

    all

--- markdown2html.py
+++ markdown2html.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 
-"""Markdown to HTML
 
-    Parameters:
-    None
+"""
+This is a script to convert a Markdown file to HTML.
 
-    Returns:
-    None
+Usage:
+    ./markdown2html.py [input_file] [output_file]
+
+Arguments:
+    input_file: the name of the Markdown file to be converted
+    output_file: the name of the output HTML file
+
+Example:
+    ./markdown2html.py README.md README.html
 """
 
 
