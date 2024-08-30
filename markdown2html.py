26b32d94cc084a9cc5279b28528f1d06b19527ea
Jocseh07 <josephmkaranja07@gmail.com>
2024-08-30 11:02:42 +0300

    task1

--- markdown2html.py
+++ markdown2html.py
@@ -18,8 +18,6 @@ import re
 
 def convert(content):
     # Convert Markdown headings to HTML
-    content = re.sub(r"^(#{1,6}) (.+)$", lambda m: f"<h{len(m.group(1))
-                                                        }>{m.group(2)}</h{len(m.group(1))}>", content, flags=re.M)
 
     return content
 
